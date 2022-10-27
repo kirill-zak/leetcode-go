797934333e358c7c50dd57d8d526141ccfc96e27
kirill-zak <kirill.zak@gmail.com>
2022-10-27 17:29:02 +0300

    Move all variants from 1480 in one file

--- problem_1480/problem_1480.go
+++ problem_1480/problem_1480.go
@@ -13,3 +13,16 @@ func runningSum(nums []int) []int {
 
 	return numsSum
 }
+
+func runningSumWithSum(nums []int) []int {
+	var result []int
+
+	sum := 0
+
+	for _, value := range nums {
+		sum += value
+		result = append(result, sum)
+	}
+
+	return result
+}

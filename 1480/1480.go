e57f568b54fe7bd327e0530352d87cb4eb835940
kirill-zak <kirill.zak@gmail.com>
2022-10-27 14:13:30 +0300

    Add solution for 1480 problem

---
+++ 1480/1480.go
@@ -0,0 +1,15 @@
+package _1480
+
+func runningSum(nums []int) []int {
+	numsSum := make([]int, len(nums))
+
+	for index, value := range nums {
+		if index > 0 {
+			numsSum[index] = numsSum[index-1] + value
+		} else {
+			numsSum[index] = value
+		}
+	}
+
+	return numsSum
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/kirill-zak/leetcode-go
+
+go 1.19
